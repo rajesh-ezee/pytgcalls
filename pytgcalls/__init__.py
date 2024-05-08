de6b5cfee49d1d0b00be75bc22a14d2537639c09
RAJESH S <87179096+rajesh-ezee@users.noreply.github.com>
2024-05-08 14:18:31 +0530

    __init__.py

--- pytgcalls/__init__.py
+++ pytgcalls/__init__.py
@@ -1,13 +1,11 @@
-from .__version__ import __version__
 from .custom_api import CustomApi
 from .pytgcalls import PyTgCalls
-from .sync import compose
+from .stream_type import StreamType
 from .sync import idle
 
 __all__ = (
-    '__version__',
-    'compose',
+    'idle',
     'CustomApi',
     'PyTgCalls',
-    'idle',
+    'StreamType',
 )
